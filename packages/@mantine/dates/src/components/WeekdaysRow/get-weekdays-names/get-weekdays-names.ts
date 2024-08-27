d2e5e4e7de64bda7eeb90fc543b2da9ee2d908a5
Alireza Moradi <aalirezamoradii@gmail.com>
2024-08-27 19:37:46 +0330

    add: Intl

--- packages/@mantine/dates/src/components/WeekdaysRow/get-weekdays-names/get-weekdays-names.ts
+++ packages/@mantine/dates/src/components/WeekdaysRow/get-weekdays-names/get-weekdays-names.ts
@@ -1,8 +1,5 @@
 import dayjs from 'dayjs';
-import jalaliday from '@zoomit/dayjs-jalali-plugin';
 import type { DayOfWeek } from '../../../types';
-
-dayjs.extend(jalaliday);
 
 interface GetWeekdaysNamesInput {
   locale: string;
@@ -15,14 +12,19 @@ export function getWeekdayNames({
   format = 'dd',
   firstDayOfWeek = 1,
 }: GetWeekdaysNamesInput) {
-  const baseDate = dayjs().calendar('gregory').day(firstDayOfWeek);
+  const baseDate = dayjs().day(firstDayOfWeek);
   const labels: Array<string | React.ReactNode> = [];
 
   for (let i = 0; i < 7; i += 1) {
     if (typeof format === 'string') {
-      labels.push(dayjs(baseDate).add(i, 'days').locale(locale).format(format));
+      labels.push(locale === 'fa'
+      ? Intl.DateTimeFormat(locale, { weekday: 'narrow' }).format(new Date().setDate(new Date().getDay() + (i + 1)))
+      : dayjs(baseDate).add(i, 'days').locale(locale).format(format));
     } else {
-      labels.push(format(dayjs(baseDate).add(i, 'days').toDate()));
+      labels.push(format(locale === 'fa'
+        ? new Date(new Date().setDate(new Date().getDay() + (i + 1)))
+        : dayjs(baseDate).add(i, 'days').toDate())
+      );
     }
   }
 
