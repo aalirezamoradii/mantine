b4a0aabeebc4facead3920f10a48c993b0625852
Alireza Moradi <a.moradi@netbar.ir>
2024-08-29 10:12:02 +0330

    fix: jalali weekdays

--- packages/@mantine/dates/src/components/WeekdaysRow/get-weekdays-names/get-weekdays-names.ts
+++ packages/@mantine/dates/src/components/WeekdaysRow/get-weekdays-names/get-weekdays-names.ts
@@ -12,19 +12,14 @@ export function getWeekdayNames({
   format = 'dd',
   firstDayOfWeek = 1,
 }: GetWeekdaysNamesInput) {
-  const baseDate = dayjs().day(firstDayOfWeek);
+  const baseDate = dayjs().day(locale === 'fa' ? 3 : firstDayOfWeek);
   const labels: Array<string | React.ReactNode> = [];
 
   for (let i = 0; i < 7; i += 1) {
     if (typeof format === 'string') {
-      labels.push(locale === 'fa'
-      ? Intl.DateTimeFormat(locale, { weekday: 'narrow' }).format(new Date().setDate(new Date().getDay() + (i + 1)))
-      : dayjs(baseDate).add(i, 'days').locale(locale).format(format));
+      labels.push(dayjs(baseDate).add(i, 'days').locale(locale).format(format));
     } else {
-      labels.push(format(locale === 'fa'
-        ? new Date(new Date().setDate(new Date().getDay() + (i + 1)))
-        : dayjs(baseDate).add(i, 'days').toDate())
-      );
+      labels.push(format(dayjs(baseDate).add(i, 'days').toDate()));
     }
   }
 
